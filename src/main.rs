09d0f4053515b48088c392af55dfb256ffe80cb0
Paul <paul21479@gmail.com>
2022-09-18 09:40:23 +1200

    added a button to the window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Button};
 
 
 const APP_ID: &str = "org.gtk_rs.fusiomappearance";
@@ -16,10 +16,27 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Super!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // connect to the clicked signal of button
+    button.connect_clicked(move |button| {
+        // Set the labe to Fusiom after button has been clicked
+        button.set_label("Fusiom!!!");
+    });
+
+
     // Create a window and set the title
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Fusiom Appearance")
+        .child(&button)
         .build();
     // Present window
     window.present();
