19b74772aa83ae479da6da3923ea41d026de36a7
Paul <paul21479@gmail.com>
2022-09-18 09:31:58 +1200

    blank window displayed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
+
+const APP_ID: &str = "org.gtk_rs.fusiomappearance";
+
 fn main() {
-    println!("Hello, world!");
+    // Create the application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to the activate signal of app
+    app.connect_activate(build_ui);
+
+    // Run the app
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Fusiom Appearance")
+        .build();
+    // Present window
+    window.present();
 }
